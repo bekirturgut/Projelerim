e38313dc88d01ae43f55f6421d656caf98e2c865
Bekir Turgut <145858150+bekirturgut@users.noreply.github.com>
2024-12-01 11:37:50 +0300

    Delete Flutter_proje2 directory

--- Flutter_proje2/widgets/ders_goster.dart
+++
@@ -1,7 +0,0 @@
-class Ders {
-  late final String ad;
-  late final double harfdegeri;
-  late final double kredidegeri;
-
-  Ders({required this.ad, required this.harfdegeri, required this.kredidegeri});
-}
